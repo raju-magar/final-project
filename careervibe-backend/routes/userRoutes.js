30b4e2d433ea8eeb47b7972fefeaf6e624a806a2
raju-magar <rajugaranjamagar52gmail.com>
2025-07-19 19:50:34 +0545

    correcting

--- careervibe-backend/routes/userRoutes.js
+++ careervibe-backend/routes/userRoutes.js
@@ -1,7 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("jsonwebtoken");
 const { getUserProfile } = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware"); // Make sure you have this
+
+router.post("?login", async (req, res) => {
+    const { username, password } = req.body;
+
+    // Find user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    } 
+
+    //  Generate JWT token
+    const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
+
+    //  Set cookie
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: false, // Set true in production with HTTPS
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 1000, // 1 hour 
+    });
+
+    res.json({ message: "Login in successfully" });
+})
 
 // Protect the route with authMiddleware
 router.get("/profile", authMiddleware, getUserProfile);
